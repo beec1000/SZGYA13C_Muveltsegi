5368a01fe6b05938be0ccb61b8d83be00c031b06
beec1000 <beec1000.minecraft@gmail.com>
2024-10-18 14:02:29 +0200

    Add project files.

---
+++ SZGYA13C_Muveltsegi/MainWindow.xaml.cs
@@ -0,0 +1,96 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Linq;
+
+namespace SZGYA13C_Muveltsegi
+{
+    public partial class MainWindow : Window
+    {
+        List<Muveltseg> muveltseg = new List<Muveltseg>();
+        public MainWindow()
+        {
+            InitializeComponent();
+            muveltseg = Muveltseg.FromFile(@"..\..\..\src\feladatok.txt");
+
+            var kerdesek = muveltseg.Select(m => m.Kerdes).ToList();
+            foreach (var k in kerdesek)
+            {
+                lib1.Items.Add(k);
+            }
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            var kerdojeles = muveltseg.FindAll(m => m.Kerdes.Contains("?"));
+            lb1.Content = $"A kérdőjeles feladatok száma: {kerdojeles.Count()}";
+        }
+
+        private void btn2_Click(object sender, RoutedEventArgs e)
+        {
+            var haromPontos = muveltseg.Where(m => m.Tananyag.Contains("kemia") && m.Pont == 3).Count();
+            lb2.Content = $"Az adatfájlban {haromPontos} db 3 pontos kémia feladat van.";
+        }
+
+        private void btn3_Click(object sender, RoutedEventArgs e)
+        {
+            var minValasz = muveltseg.Min(m => m.Valasz);
+            var MaxValasz = muveltseg.Max(m => m.Valasz);
+            lb3.Content = $"A válaszok számértéke {minValasz} és {MaxValasz} között terjed.";
+        }
+
+        private void btn4_Click(object sender, RoutedEventArgs e)
+        {
+            var temakorok = muveltseg.OrderBy(m => m.Tananyag).Select(m => m.Tananyag).Distinct();
+            foreach (var t in temakorok)
+            {
+                lib2.Items.Add(t);
+            }
+        }
+
+        private void btn5_Click(object sender, RoutedEventArgs e)
+        {
+            var kereses = muveltseg.Where(m => m.Kerdes.Contains(tbx1.Text)).ToList();
+            if(kereses.Count != 0)
+            {
+                foreach (var k in kereses)
+                {
+                    lib3.Items.Add(k.Kerdes);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ez a szó/szórészlet nem szerepel egyik kérdésben sem!", "ERROR", MessageBoxButton.OK, MessageBoxImage.Warning);
+                tbx1.Text = string.Empty;
+            }
+            
+        }
+
+        private void btn6_Click(object sender, RoutedEventArgs e)
+        {
+            //itt randommal kellene, de mivel arra nem jöttem rá, ezért az első kérdéssel dolgoztam.
+            Random rnd = new Random();
+            var kereses = muveltseg.Where(m => m.Kerdes.Contains(tbx1.Text)).ToList();
+            //var randomI = rnd.Next(1, kereses.Count() + 1);
+            lb4.Content = muveltseg.Select(m => m.Kerdes).First();
+            if (int.Parse(tbx2.Text) == muveltseg.Select(m => m.Valasz).First())
+            {
+                lb5.Content = $"A válasz {muveltseg.Select(m => m.Pont).First()} pontot ér.";
+            }
+            else
+            {
+                lb5.Content = $"A válasz 0 pontot ér.";
+            }
+            
+        }
+    }
+}
---
+++ SZGYA13C_Muveltsegi/Muveltseg.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SZGYA13C_Muveltsegi
+{
+    internal class Muveltseg
+    {
+        public string Kerdes { get; set; }
+        public int Valasz { get; set; }
+        public int Pont { get; set; }
+        public string Tananyag { get; set; }
+
+        public Muveltseg(string kerdes, int valasz, int pont, string tanagyag) 
+        { 
+            Kerdes = kerdes;
+            Valasz = valasz;
+            Pont = pont;
+            Tananyag = tanagyag;
+        }
+
+        public static List<Muveltseg> FromFile(string path)
+        {
+            List<Muveltseg> muveltseg = new List<Muveltseg>();
+
+            string[] line = File.ReadAllLines(path);
+
+            foreach (var l in line)
+            {
+                string[] s = l.Split(';');
+
+                string Kerdes = s[0];
+                int Valasz = int.Parse(s[1]);
+                int Pont = int.Parse(s[2]);
+                string Tananyag = s[3];
+
+                Muveltseg muveltsegek = new(Kerdes, Valasz, Pont, Tananyag);
+
+                muveltseg.Add(muveltsegek);
+            }
+            return muveltseg;
+        }
+
+        public override string ToString()
+        {
+            return $"{Kerdes};{Valasz};{Pont};{Tananyag}";
+        }
+    }
+}
